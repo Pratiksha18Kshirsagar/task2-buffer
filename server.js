26119f469ed9c2b8b8465538480e605240b8e070
Pratiksha18Kshirsagar <kshirsagarpratiksha1820@gmail.com>
2025-12-17 14:15:40 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === '/' ) {
+    fs.readFile('message.txt', (err, data) => {
+      const messages = data ? data.split('\n').filter(Boolean).reverse() : [];
+
+      const messageList = messages
+        .map(msg => <p>${msg}</p>)
+        .join('');
+
+      res.setHeader('Content-Type', 'text/html');
+      res.end(`
+        <html>
+          <head>
+            <title>Messages</title>
+          </head>
+          <body>
+            <h2>Messages</h2>
+            ${messageList}
+            <hr/>
+            <form action="/message" method="POST">
+              <input type="text" name="message" />
+              <button type="submit">Send</button>
+            </form>
+          </body>
+        </html>
+      `);
+    });
+  }
+
+  else if (req.url === '/message') {
+    const body = [];
+
+    req.on('data', chunk => body.push(chunk));
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+
+      fs.appendFile('message.txt', message + '\n', () => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+      });
+    });
+  }
+
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Page Not Found</h1>');
+  }
+
+});
+
+server.listen(3000);
